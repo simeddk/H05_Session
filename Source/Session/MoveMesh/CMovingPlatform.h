4a430e49e967a5c1392ccfcfc1420ba423542496
KS Kim <65766852+simeddk@users.noreply.github.com>
2023-07-05 15:40:19 +0900

    0705_MovingPlatform

--- Source/Session/MoveMesh/CMovingPlatform.h
+++ Source/Session/MoveMesh/CMovingPlatform.h
@@ -18,7 +18,22 @@ protected:
 private:
 	virtual void Tick(float DeltaTime) override;
 
+public:
+	void AddActiveTrigger();
+	void SubActiveTrigger();
+
 private:
 	UPROPERTY(EditAnywhere)
-		float Speed = 300.f;
+		float Speed = 60.f;
+
+	UPROPERTY(EditAnywhere, meta = (MakeEditWidget))
+		FVector TargetLocation = FVector(200, 0, 0);
+
+	//if this value more than 1, this platform will moving
+	UPROPERTY(EditInstanceOnly)
+		int32 ActiveTrigger = 0;
+
+private:
+	FVector GlobalStartLocation;
+	FVector GlobalTargetLocation;
 };
--- Source/Session/Session.Build.cs
+++ Source/Session/Session.Build.cs
@@ -9,5 +9,6 @@ public class Session : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicIncludePaths.Add(ModuleDirectory);
 	}
 }
